91e9a7df3ff4cd5b3811749c10a5f62010e79f0a
Sharvzz <loganathansharvesh14@gmail.com>
2025-07-16 11:14:13 +0530

    backend is running

--- backend/backend-env/bin/activate.fish
+++ backend/backend-env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/sharvesh/Documents/resume-backend/backend-env
+set -gx VIRTUAL_ENV /home/sharvesh/Desktop/ai-resume-analyzer/backend/backend-env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
